1a15ed2326de8f0c4636d1f6c66823e1a17bf7c0
BerikU <113161304+BerikU@users.noreply.github.com>
2022-11-14 20:32:19 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿string[] CreateArray(int size)
+{
+    string[] array = new string[size];
+
+    Console.WriteLine("Make array: ");
+
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Insert {i + 1} element: ");
+        array[i] = Console.ReadLine();
+    }
+    Console.WriteLine();
+    return array;
+}
+
+void ShowArray(string[] array)
+{
+    Console.WriteLine("Array is: ");
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + ", ");
+
+    Console.WriteLine();
+}
+
+int FindSizeOfNewArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) count++;
+    }
+    return count;
+}
+
+string[] CreateNewArray(string[] array, int size)
+{
+    string[] newArray = new string[size];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+            
+    }
+    return newArray;
+} 
+
+Console.WriteLine("Set a size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] myArray = CreateArray(size);
+
+ShowArray(myArray);
+
+int newSize = FindSizeOfNewArray(myArray);
+
+Console.WriteLine();
+
+string[] myArray2 = CreateNewArray(myArray, newSize);
+ShowArray(myArray2);
